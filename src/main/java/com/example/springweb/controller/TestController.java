8806571b2d457eb178c1ddeab832a4931e4c3001
jtzhxx <1650917692@qq.com>
2022-08-16 17:01:08 +0800

    test

---
+++ src/main/java/com/example/springweb/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.springweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String getTest(){
+        return "zxx";
+    }
+}
